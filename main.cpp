705841f61459caf0f784b6a17164c0a8d48fe62c
Yusuf Pisan <pisan@uw.edu>
2021-01-13 20:06:51 -0800

    start

--- main.cpp
+++ main.cpp
@@ -44,7 +44,6 @@ public:
   TreeNode *deleteNode(TreeNode *root, int key) {
     if (!root)
       return nullptr;
-    PV2(key, root);
     if (key < root->val) {
       root->left = deleteNode(root->left, key);
     } else if (key > root->val) {
@@ -54,7 +53,7 @@ public:
       if (root->left && root->right) {
         // have 2 children
         // find smallest larger than root
-        TreeNode *rt = root->right;
+        auto *rt = root->right;
         while (rt->left) {
           rt = rt->left;
         }
@@ -62,22 +61,67 @@ public:
         // need to delete rt from right branch
         root->right = deleteNode(root->right, rt->val);
       } else if (root->left) {
-        root = root->left;
+        return root->left;
       } else {
-        root = root->right;
+        return root->right;
       }
+    }
+    return root;
+  }
+
+  // sticks the left-subtree from key under the inorder-successor
+  // creates very unbalanced trees quickly
+  TreeNode *deleteNode2(TreeNode *root, int key) {
+    if (!root)
+      return nullptr;
+    if (root->val == key) {
+      if (root->left && root->right) {
+        auto *newroot = root->right;
+        while (newroot->left)
+          newroot = newroot->left;
+        PV1(newroot->val);
+        newroot->left = root->left;
+        newroot = root->right;
+        return newroot;
+      } else if (root->left) {
+        return root->left;
+      } else // root->right must exist
+        return root->right;
+    } else if (root->val > key) {
+      root->left = deleteNode(root->left, key);
+    } else {
+      root->right = deleteNode(root->right, key);
     }
     return root;
   }
 };
 
+bool sameTree(TreeNode *a, TreeNode *b) {
+  if (!a && !b)
+    return true;
+  return (a && b && a->val == b->val && sameTree(a->left, b->left) &&
+          sameTree(a->right, b->right));
+}
 void test1() {
   Solution s;
-  vector<int> v{5, 3, 6, 2, 4, INT_MIN, 7};
-  auto *tx = new TreeNode(v);
-  cout << *tx << endl;
-  TreeNode *result = s.deleteNode(tx, 3);
-  cout << *result << endl;
+  //   vector<int> v{5, 3, 6, 2, 4, INT_MIN, 7};
+  vector<int> v{10, 3, 20, 2, 4, 15, 30, 1, INT_MIN, INT_MIN, INT_MIN, 12, 16};
+  for (int elt : v) {
+    if (elt == INT_MIN)
+      continue;
+    auto *tx = new TreeNode(v);
+    auto *ty = new TreeNode(v);
+    auto *t1 = s.deleteNode(tx, elt);
+    cout << t1 << endl;
+    auto *t2 = s.deleteNode2(ty, elt);
+    cout << t2 << endl;
+    return;
+    if (!sameTree(t1, t2)) {
+      cout << t1 << endl;
+      cout << t2 << endl;
+      return;
+    }
+  }
 }
 
 void test2() {}
